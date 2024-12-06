0875d1e95fb47be4b64f0d2dfae2052dd1d822af
Juan Villalobos <juan.villalobos1992@gmail.com>
2024-12-06 13:44:26 +0100

    adjust contrast levels for anchor tags and buttons in light and dark mode

--- src/app/materialTheme.tsx
+++ src/app/materialTheme.tsx
@@ -5,10 +5,17 @@ const theme = createTheme({
   palette: {
     primary: {
       main: "#911B2E",
+      light: "#B62238",
+      dark: "#741625",
+      contrastText: "#FDF9F9",
     },
     secondary: {
-      main: "#2DBDBD",
+      main: "#225252",
+      light: "#DCF2F1",
+      dark: "#1D4140",
+      contrastText: "#F5FBFB",
     },
+    contrastThreshold: 7.0,
   },
   typography: {
     fontFamily: ["Open Sans", "Domine"].join(","),
@@ -18,10 +25,11 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           borderRadius: 28,
-          height: 52,
+          height: 44,
           fontSize: 18,
+          fontWeight: 700,
+          textTransform: "none",
         },
-        colorPrimary: "#911B2E",
       },
     },
   },
--- src/template/SiteNavigation/SiteNavigation.tsx
+++ src/template/SiteNavigation/SiteNavigation.tsx
@@ -1,13 +1,17 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./SiteNavigation.module.scss";
-import { Button } from "@mui/material";
+import { Button, useMediaQuery } from "@mui/material";
 import NavigationMenu from "@/components/molecules/NavigationMenu/NavigationMenu";
 import NavigationItem from "@/components/atoms/NavigationItem/NavigationItem";
 
 interface ISiteNavigationProps {}
 
 const SiteNavigation = ({}): ISiteNavigationProps => {
+  // const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  // console.log("prefersDarkMode", prefersDarkMode);
+
   return (
     <header className="w-full flex flex-wrap justify-center">
       <div className="w-full max-w-screen-lg xl:max-w-screen-2xl mt-2">
@@ -42,7 +46,7 @@ const SiteNavigation = ({}): ISiteNavigationProps => {
                 <Button
                   className="w-full mb-3"
                   href="https://mailchi.mp/f5a5cde72458/frontend-development-with-juanton"
-                  variant="outlined"
+                  variant="contained"
                   color="primary"
                   target="_blank"
                   rel="nofollower"
